990842a2bb297a69209411b6a4cd53012b254826
Danny <dannytran4@cmail.carleton.ca>
2025-10-16 23:28:48 -0400

    Remove unnecessary example code

--- docs/Freenove_ESP32_S3_WROOM_Board_Lite-main/Python/Python_Firmware/mac.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import time
-
-os.system("python3 esptool/esptool.py --chip esp32s3  erase_flash")
-
-os.system("python3 esptool/esptool.py --chip esp32s3 --baud 2000000 write_flash -z 0 ESP32_GENERIC_S3-SPIRAM_OCT-20240602-v1.23.0.bin")
