a19d50a8d9292073f8a4e8751f1e55797f596681
thurk <thurka.thyagarajah@gmail.com>
2025-10-25 22:38:00 -0400

    Updating memory value for accelerometer task

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -5,7 +5,7 @@
 
 
 void app_main(void) {
-	xTaskCreate(accelerometer_task, "Accelerometer Task", 2048, NULL, 5, NULL); 
+	xTaskCreate(accelerometer_task, "Accelerometer Task", 2100, NULL, 5, NULL); 
 
 	while(1) vTaskDelay(pdMS_TO_TICKS(10)); // Yields to other tasks; 
 }
